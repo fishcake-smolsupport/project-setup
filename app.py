16e08212bd97b22a618551f99b2b5282d1ab15c5
fischcakewithstats <yamwengchay@gmail.com>
2024-06-24 00:29:21 +0800

    Add config support

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
 
 @app.route("/")
 def index():
-    return "This is yet another version!"
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    secret_key = app.config.get("SECRET_KEY")
+    return f"The configured secret key is {secret_key}."
